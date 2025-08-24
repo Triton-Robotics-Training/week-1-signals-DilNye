d34573b8c0a282ecabd05c2a4b493971a990bd86
DilNye <dilshodbekbnamozov@gmail.com>
2025-08-24 14:33:17 -0700

    Update week1_exercise2.cpp

--- week1_exercise2.cpp
+++ week1_exercise2.cpp
@@ -11,25 +11,37 @@ int main() {
     while (1) {
         printf("Week 1 Exercise 2");
         
-        if (anInput > 0) {
-            output = 1;
+        double OnTime = (anInput)*2000;
         
-            double OnTime = (anInput/3.3)*2000;
-        
-            double OffTime = 2000-OnTime;
-        
+    double OffTime = 2000-OnTime; 
     
-            wait_ms(OnTime);
+    
+    if (OnTime > 0 && OnTime <= 1500) {
+        output = 1;
+        wait_ms(OnTime);
         
-            output = 0;
+        output = 0;
+        wait_ms(OffTime-500); //500 due to the 500ms wait time
+    }
+    /* I noticed that when OnTime > 1500 ms, the 500ms wait time would get in the way of the period,
+       so I corrected that using the next conditional. It could probably be more optimized
+       if I integrated it into the above conditional, but this seemed more simple to write out */
+    else if (OnTime > 1500) { 
+        output = 1;
+        wait_ms(100); //The 100ms is arbitrary, I just needed to have the off time in the middle of the period so that it doesn't run into the 500ms at the end
         
-            wait_ms(OffTime);
-        
+        if (OffTime > 0) {output = 0;
+        wait_ms (OffTime);
         }
-        else {
-            output = 0;
-        }
         
+        output = 1;
+        wait_ms (OnTime-600-OffTime); //600 here due to 100 ms from earlier and the required 500ms between cycles
+        
+    }
+    
+    else {
+        output = 0;
+    }   
         // MAKE SURE THERE IS ALWAYS A WAIT ON THE SIM OR IT WILL CRASH
         wait_ms(500); 
     }
